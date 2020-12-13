e131543cfc31341108f23ff80b932cc0a3916d53
MyroslavPro <myropro@gmail.com>
2020-12-13 14:30:21 +0200

    Fixed the header

--- lab5d.h
+++ lab5d.h
@@ -1,3 +1,4 @@
+#pragma once
 enum kind 
 {
     DOG=1,
@@ -9,66 +10,56 @@ enum kind
 class Pet
 {
     private:
-    string name;
-    int age;
-    string greeting;
-    int mass;
-    string breed;
-   // return if they're friends
-   /*
-    friend void areFriends(Pet* Pet_Array,int Number);
-    friend void ShowMatrix_Name_and_Age ( Pet* Pet_Array ,int N);
-    friend void SortMatrix (Pet* Pet_Array ,int N);
-    */
-   
+        string name;
+        int age;
+        string greeting;
+        int mass;
+        string breed;
     public:
-    bool isPolite()
+        bool isPolite()
         {
             if (getGreeting() == "Hello")
             {
-            return true;
-            
+                return true;
             }
         }  
 
-    string getKind(kind k)
-    {
-        if (k==DOG)
+        string getKind(kind k)
         {
-            return "Doggy";
+            if (k==DOG)
+            {
+                return "Doggy";
+            }
+            if (k==CAT)
+            {
+                return "Cat";
+            }
+            if (k==BIRD)
+            {
+                return "Bird";
+            }
+            if (k==FISH)
+            {
+                return "Fish";
+            }
         }
-        if (k==CAT)
-        {
-            return "Cat";
-        }
-        if (k==BIRD)
-        {
-            return "Bird";
-        }
-        if (k==FISH)
-        {
-            return "Fish";
-        }
-    }
-    Pet();
-    Pet(string n,int a,string g,int m,string b);
-    ~Pet();
-    
-    string getName();
-    int getAge();
-    string getGreeting();
-    int getMass();
-    string getBreed();
+        Pet();
+        Pet(string newName,int newAge,string newGreeting,int newMass,string newBreed);
+        ~Pet();
 
-    void setName(string Name);
-    void setAge(int Age);   
-    void setGreeting(string Greeting);
-    void setMass(int Mass);   
-    void setBreed(string Breed);
+        string getName();
+        int getAge();
+        string getGreeting();
+        int getMass();
+        string getBreed();
+
+        void setName(string newName);
+        void setAge(int newAge);   
+        void setGreeting(string newGreeting);
+        void setMass(int newMass);   
+        void setBreed(string newBreed);
 
 };
-
-
 
 Pet::Pet()
 {
@@ -78,146 +69,138 @@ Pet::Pet()
     mass=0;
     breed ="Some";
 }
-Pet::Pet(string n,int a,string g,int m,string b)
+Pet::Pet(string newName,int newAge,string newGreeting,int newMass,string newBreed)
 {
-    name = n;
-    age= a;
-    greeting = g;
-    mass=m;
-    breed =b;
+    name = newName;
+    age= newAge;
+    greeting = newGreeting;
+    mass=newMass;
+    breed =newBreed;
 }
 Pet::~Pet(){}
 
-    string Pet::getName()
-    {
-        return name;
-    }
-    int Pet::getAge()
-    {
-        return age;
-    }
-    string Pet::getGreeting()
-    {
-        return greeting;
-    }
-    int Pet::getMass()
-    {
-        return mass;
-    }
-    string Pet::getBreed()
-    {
-        return breed;
-    }
-   
-    void Pet::setName(string Name)
-    {
-        name= Name;
-    }
-    
-    void Pet::setAge(int Age)
-    {
-        age = Age;
-    }
-    void Pet::setGreeting(string Greeting)
-    {
-        greeting=Greeting;
-    }
-    void Pet::setMass(int Mass)
-    {
-        mass=Mass;
-    }
-    void Pet::setBreed(string Breed)
-    {
-        breed=Breed;
-    }
+
+string Pet::getName()
+{
+    return name;
+}
+int Pet::getAge()
+{
+    return age;
+}
+string Pet::getGreeting()
+{
+    return greeting;
+}
+int Pet::getMass()
+{
+    return mass;
+}
+string Pet::getBreed()
+{
+    return breed;
+}
 
 
-
-///////////////
-
-
-
+void Pet::setName(string newName)
+{
+    name= newName;
+}
+void Pet::setAge(int newAge)
+{
+    age = newAge;
+}
+void Pet::setGreeting(string newGreeting)
+{
+    greeting=newGreeting;
+}
+void Pet::setMass(int newMass)
+{
+    mass=newMass;
+}
+void Pet::setBreed(string newBreed)
+{
+    breed=newBreed;
+}
 
 class Home
 {
     private:
-    int number;
-    friend void areFriends(Pet* Pet_Array,int Number);
-    friend void ShowMatrix_Name_and_Age ( Pet* Pet_Array ,int N);
-    friend void SortMatrix (Pet* Pet_Array ,int N);
-    friend void function_put(Pet* Pet_Array,int N);
+        int number;
+        friend void areFriends(Pet* petsArray,int number);
+        friend void showMatrixNameAndAge ( Pet* petsArray ,int number);
+        friend void sortMatrixFromSmallToBig (Pet* petsArray ,int number);
+        friend void function_put(Pet* petsArray,int number);
     public:
-    Pet *Pet_Array;
-    Home();
-    Home(int Number);
-    ~Home();
-    
-    void setNumber(int n)
-    {
-        number=n;
-    }
-    int getNumber()
-    {
-        return number;
-    }
+        Pet *petsArray;
+        Home();
+        Home(int number);
+        ~Home();
+
+        void setNumber(int newNumber)
+        {
+            number=newNumber;
+        }
+        int getNumber()
+        {
+            return number;
+        }
 
 };   
 
 Home::Home()
 {
-        setNumber(0);   
+    setNumber(0);   
 }
 
-Home::Home(int N)
+Home::Home(int number)
 {
-   setNumber(N);       
-   Pet_Array = new Pet[N];
+    setNumber(number);       
+    petsArray = new Pet[number];
 }
 
 Home::~Home()
 {
-    delete[] Pet_Array;
+    delete[] petsArray;
 }
 
 
-
-///
-
-void function_put(Pet* Pet_Array,int N)
+void function_put(Pet* petsArray,int number)
 {
-    for (int i=0;i< N;i++)
+    for (int i=0;i< number;i++)
     {
         cout<<""<<endl; 
         string n;
         cin>>n;
-        Pet_Array[i].setName(n);
+        petsArray[i].setName(n);
 
         int a;
         cin>>a;
-        Pet_Array[i].setAge(a);
+        petsArray[i].setAge(a);
 
         string g;
         cin>>g;
-        Pet_Array[i].setGreeting(g);
+        petsArray[i].setGreeting(g);
 
         int m;
         cin>>m;
-        Pet_Array[i].setMass(m);
+        petsArray[i].setMass(m);
         
         string b;
         cin>>b;
-        Pet_Array[i].setBreed(b);
+        petsArray[i].setBreed(b);
     }
 }
-void areFriends(Pet *Pet_Array,int N)
+
+void areFriends(Pet *petsArray,int number)
 {
-    for(int i = 0;i<N-1;i++)
+    for(int i = 0;i<number-1;i++)
     {
-     for(int j = i+1;j<N;j++)
+        for(int j = i+1;j<number;j++)
         {
-        if (abs(Pet_Array[i].getAge() - Pet_Array[j].getAge())< 2)
+            if (abs(petsArray[i].getAge() - petsArray[j].getAge())< 2)
             {
-                cout<<Pet_Array[i].getName()<<" is a friend of"<<Pet_Array[j].getName()<<endl; 
+                cout<<petsArray[i].getName()<<" is a friend of "<<petsArray[j].getName()<<endl; 
             }
         }
     }
@@ -225,34 +208,29 @@ void areFriends(Pet *Pet_Array,int N)
 }
 
 
-void ShowMatrix_Name_and_Age (Pet* Pet_Array ,int N)
+void showMatrixNameAndAge (Pet* petsArray ,int number)
 {
-    for(int i = 0;i<N;i++)
+    for(int i = 0;i<number;i++)
     {
-            cout<<Pet_Array[i].getName()<<": his age - "<<Pet_Array[i].getAge()<<endl; 
-        
+        cout<<petsArray[i].getName()<<": his age - "<<petsArray[i].getAge()<<endl; 
     }
     cout<<endl;
 }
 
-void SortMatrix (Pet *Pet_Array,int N)
+void sortMatrixFromSmallToBig(Pet *petsArray,int number)
 {
-    
-    Pet Mins=Pet_Array[0];
-    
-    for(int i = 0;i<N;i++)
+    Pet Mins=petsArray[0];
+    for(int i = 0;i<number;i++)
     {
-        for(int j = i+1;j<N;j++)
+        for(int j = i+1;j<number;j++)
         {
-
-              if (Pet_Array[j].getAge()< Mins.getAge())
-                {
-                    Mins=Pet_Array[i];
-                    Pet_Array[i]=Pet_Array[j];
-                    Pet_Array[j]=Mins;
-                }
-
-           }
+            if (petsArray[j].getAge()< Mins.getAge())
+            {
+                Mins=petsArray[i];
+                petsArray[i]=petsArray[j];
+                petsArray[j]=Mins;
+            }
+        }
     }
     cout<<"It's been sorted"<<endl;
     cout<<endl;
